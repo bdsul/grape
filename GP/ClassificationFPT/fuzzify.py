8e55a867795e9904fdae5b511e202724583fcea7
Allan de Lima <49134097+danozu@users.noreply.github.com>
2024-06-20 20:47:11 +0100

    Update fuzzify.py

--- GP/ClassificationFPT/fuzzify.py
+++ GP/ClassificationFPT/fuzzify.py
@@ -137,52 +137,44 @@ def fuzzifyDataFrame(DataFrame, nSets, matrixDomain):
     nonCategorical = [] #eliminate non-categorical data in dataCategorical
     
     for i in range(numVariables):
-        if matrixDomain[i][0] == 'c': #dados categóricos
+        if matrixDomain[i][0] == 'c': #categorical data
             nonReal.append(i)
-        else: #dados não categóricos
+        else: #non-categorical data
             nonCategorical.append(i)
     
     dataReal.drop(dataReal.columns[nonReal], axis=1, inplace=True)
     dataCategorical.drop(dataCategorical.columns[nonCategorical], axis=1, inplace=True)
     
-    #dimensões da parte não categórica do dataframe
-    #dataReal = df.select_dtypes(include=['float64'])
-    _, numColumnsReal = np.shape(dataReal) #o número de linhas não interessa, pois é o mesmo em validNumberRows
+    _, numColumnsReal = np.shape(dataReal) #the number of lines doesn't matter, because it's the same in validNumberRows
         
-    #dimensões da parte categórica do dataframe
-    #dataCategorical = df.select_dtypes(include=['object'])
     _, numColumnsCategorical = np.shape(dataCategorical)
         
-    #nomes das variáveis
-    nameCol = df.columns #todos os nomes
-    nameColReal = dataReal.columns #nomes das variáveis reais
-    nameColCategorical = dataCategorical.columns #nomes das variáveis categóricas
+    nameCol = df.columns #names of all features
+    nameColReal = dataReal.columns #names of fuzzy features
+    nameColCategorical = dataCategorical.columns #names of categorical features
     
     arraySets = np.empty(numColumns, dtype=int)
-    #arraySets armazena o número de conjuntos em que cada variável se dividirá
-    #Caso a entrada nSets seja um valor inteiro, quer dizer que todas as variáveis não categóricas
-    #se  dividirão no mesmo número de conjuntos
-    #Caso seja um array, cada posição faz referência a uma coluna com dados no dataframe
-    #A posição referente a uma coluna categórica deve conter exatamente o mesmo número de 
-    #categorias em que os dados estão divididos
-    j, k = 0, 0 #índices dos nomes de colunas com dados reais e categóricos, respectivamente
+    #arraySets keeps the number of sets to split each feature
+    #If nSets is integer, all non-categorical features will be splitted with the same number of sets
+    #If it's an array, each position refers to each column
+    #The position reffering to a categorical feature should have the number of categories of that feature
+    j, k = 0, 0
     if type(nSets) == int:
         if nSets < 2:
             print("Number of sets must be greater than or equal to 2")
             return
         else:
             for i in range(numColumns):
-                if numColumnsReal > j: #se ainda há colunas reais a verificar
+                if numColumnsReal > j: #if there are any more columns to verify
                     if nameCol[i] == nameColReal[j]:
                         arraySets[i] = nSets
                         j += 1
-                if numColumnsCategorical > k: #idem para colunas categóricas
+                if numColumnsCategorical > k: #same for non-categorical
                     if nameCol[i] == nameColCategorical[k]:
-                        #se o número de categorias indicado na matriz é realmente o número de categorias em que os dados se dividem
                         arraySets[i] = len(matrixDomain[i]) - 1 #-1 because the first position is 'n' or 'c'
                         k += 1
                         
-    else: #se o valor passado foi um vetor
+    else: #if it's an array
         nSetsSize = len(nSets)
         if numVariables != nSetsSize:
             print("Size of the array nSets must be equal to the number of variables.")
@@ -193,45 +185,35 @@ def fuzzifyDataFrame(DataFrame, nSets, matrixDomain):
                 return
             arraySets[i] = nSets[i]
     
-    #é necessário passar os dados do dataframe para uma matriz, pois o dataframe pode ter
-    #tido linhas deletadas (por falta de dados), o que dificulta o seu endereçamento
-    #Nesta matriz, as posições do dataframe com dados deletados ficarão zeradas
+    #if some rows in the dataframe were removed (for missing values), the pointing could be difficult,
+    #so, we send the values of the dataframe to a matrix
+    #The respective positions with removed data in the dataframe will get zero in the matrix
     matrixDataReal = np.zeros([numRows,numColumnsReal], dtype=float)
     
-    sumSets = int(sum(arraySets)) #total de conjuntos a dividir as variáveis
+    sumSets = int(sum(arraySets)) #total of sets
     for i in range(numColumns):
         if matrixDomain[i][0] == 'c' and arraySets[i] == 2:
             sumSets -= 1
-    pertinenceMatrix = np.zeros([numRows,sumSets], dtype=float) #matriz de pertinência
-    pertinenceMatrixDF = {} #dataframe final
+    pertinenceMatrix = np.zeros([numRows,sumSets], dtype=float)
+    pertinenceMatrixDF = {} #final dataframe
     
-    i = 0 #não uso o index para referenciar, porque pode ter linhas deletadas
+    i = 0 #we cannot use the index as pointer because of possible missing rows
     for index, row in dataReal.iterrows(): 
         for j in range(numColumnsReal):
             matrixDataReal[validIndexes[i]][j] = row[nameColReal[j]]
-       # print(index,i)
         i += 1
     
-    #hora de preencher a matriz de pertinência
-    actualColumn = 0 #coluna que será preenchida
+    actualColumn = 0 #column to the filled now
     actualIndexSets = 0
     for i in range(numColumns):
-        if matrixDomain[i][0] == 'c': #dados categóricos
+        if matrixDomain[i][0] == 'c': #categorical data
             if arraySets[i] != 2:
                 arrayCategories = []
                 for j in range(arraySets[i]):
                     arrayCategories.append(matrixDomain[i][j+1])
-#                arrayCategories = np.empty(arraySets[i], dtype=str) #arraySets[i] é o número de categorias
-#                arrayCategories = df[nameCol[i]].unique() #nome de cada categoria da coluna i
-                #Se as categorias são números, especialmente 0 e 1, e pela leitura das classes, 
-                #tenham sido ordenadas como 1 e 0, vai dar erro, se o que se espera é uma fuzzificação 
-                #0=10 e 1=01, então para isso ordena-se antes
-#                arrayCategories = sorted(arrayCategories) 
-                #for j in range(arraySets[i]): #necessário, caso as categorias sejam números
-                #    arrayCategories[j] = str(df[nameCol[i]].unique()[j])
-                j = 0 #posição no vetor de índices válidos
-                for index in range(validNumberRows): #para cada linha válida
-                    for k in range(arraySets[i]): #para cada conjunto da variável atual
+                j = 0 #position in the array of valid data
+                for index in range(validNumberRows): #for each valid row
+                    for k in range(arraySets[i]): #for each set of the current feature
                         if arrayCategories[k] == str(df.loc[validIndexes[j]][i]): #quando as categorias são números, às vezes ocorrem problemas. Verificar se str() corrigiu
                             #se o objeto pertence à categoria atual, a posição na matriz de pertinência será 1
                             pertinenceMatrix[validIndexes[j],actualColumn+k] = 1
