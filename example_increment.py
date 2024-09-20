bbaff693749c463bcf6dad957c287d74efcfb109
Allan de Lima <49134097+danozu@users.noreply.github.com>
2024-09-20 16:14:52 +0100

    Update example_increment.py

--- example_increment.py
+++ example_increment.py
@@ -28,39 +28,14 @@ creator.create('Individual', grape.Individual, fitness=creator.FitnessMin)
 
 toolbox.register("populationCreator", grape.sensible_initialisation, creator.Individual) 
 #toolbox.register("populationCreator", grape.random_initialisation, creator.Individual) 
-#toolbox.register("populationCreator", grape.PI_Grow, creator.Individual) 
-
-#toolbox.register("evaluate", fitness_eval)
-
-# Tournament selection:
-toolbox.register("select", tools.selTournament, tournsize=7) #selLexicaseFilter
-
-# Single-point crossover:
-toolbox.register("mate", grape.crossover_onepoint)
-
-# Flip-int mutation:
-toolbox.register("mutate", grape.mutation_int_flip_per_codon)
 
 POPULATION_SIZE = 10
 MAX_INIT_TREE_DEPTH = 6
 MIN_INIT_TREE_DEPTH = 4
 
-MAX_GENERATIONS = 200
-P_CROSSOVER = 0.8
-P_MUTATION = 0.01
-ELITE_SIZE = 0#round(0.01*POPULATION_SIZE) #it should be smaller or equal to HALLOFFAME_SIZE
-HALLOFFAME_SIZE = 1#round(0.01*POPULATION_SIZE) #it should be at least 1
-
-MIN_INIT_GENOME_LENGTH = 95#*6
-MAX_INIT_GENOME_LENGTH = 115#*6
-random_initilisation = False #put True if you use random initialisation
-
 CODON_CONSUMPTION = 'lazy'
 GENOME_REPRESENTATION = 'list'
-MAX_GENOME_LENGTH = None#'auto'
 
-MAX_TREE_DEPTH = 35 #equivalent to 17 in GP with this grammar
-MAX_WRAPS = 0
 CODON_SIZE = 255
 
 random.seed(RANDOM_SEED) 
